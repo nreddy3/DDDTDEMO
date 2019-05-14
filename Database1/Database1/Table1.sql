1317aaa3410c0fd36ebbcfaed172a4d10f914fe3
Naresh Reddy <naresh.reddy1@hp.com>
2019-05-14 16:51:20 +0530

    New commit

---
+++ Database1/Database1/Table1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [email] NVARCHAR(50) NULL, 
+    [firstname] NVARCHAR(50) NULL, 
+    [lastname] NVARCHAR(50) NULL, 
+    [fathername] NVARCHAR(50) NULL
+)
